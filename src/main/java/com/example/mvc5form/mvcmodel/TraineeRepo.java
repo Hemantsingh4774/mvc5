1be8876e0f8761c236f15cd1242ccbc45390c662
Hemantsingh4774 <hemant.singh_cs.ccv19@gla.ac.in>
2023-05-31 01:11:08 +0530

    Revert "Added Form functionality to trainee"
    This reverts commit 0f667bb98daee3812a0b1be746a1420ab36cf4a5.

--- src/main/java/com/example/mvc5form/mvcmodel/TraineeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.mvc5form.mvcmodel;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TraineeRepo extends JpaRepository<Trainee,Long> {
-
-}
